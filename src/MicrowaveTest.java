304d2d90b72eb50a5eb8a1f62a24e7a34afd05d9
Libii <libisin@gmail.com>
2020-01-23 17:32:33 -0800

    Removed Answers

--- src/MicrowaveTest.java
+++ src/MicrowaveTest.java
@@ -1,49 +1,29 @@
-import static org.junit.Assert.*;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class MicrowaveTest {
 
     @Test
     public void switchPowerLevel() {
         Microwave testMicrowave = new Microwave();
-
-        testMicrowave.switchPowerLevel();
-        assertEquals("Cooking for 0 seconds at Power Level 2", testMicrowave.pressStart());
+        assertEquals("Cooking for 0 seconds at Power Level 1",testMicrowave.pressStart());
 
     }
 
     @Test
     public void reset() {
-        Microwave testMicrowave = new Microwave();
-
-        //Switch to 1 minute
-        testMicrowave.add30Seconds();
-        testMicrowave.add30Seconds();
-
-        //Reset Microwave
-        testMicrowave.reset();
-
-        //test
-        assertEquals("Cooking for 0 seconds at Power Level 1", testMicrowave.pressStart());
-
     }
 
     @Test
     public void add30Seconds() {
-        Microwave testMicrowave = new Microwave();
-
-        //Switch to 1 minute
-        testMicrowave.add30Seconds();
-        testMicrowave.add30Seconds();
-
-        //test
-        assertEquals("Cooking for 60 seconds at Power Level 1", testMicrowave.pressStart());
     }
 
     @Test
     public void pressStart() {
         Microwave testMicrowave = new Microwave();
-
-        assertEquals("Cooking for 0 seconds at Power Level 1", testMicrowave.pressStart());
+        assertEquals("Cooking for 0 seconds at Power Level 1",testMicrowave.pressStart());
     }
+
+
 }
